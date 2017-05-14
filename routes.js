16b420a06fe283c61d8e52a2bd6add82541e0d9f
Andrew <andy@andycondon.com>
2017-05-14 19:56:47 +1200

    add skeleton. Return db content. Build initial routes

--- routes.js
+++ routes.js
@@ -3,11 +3,34 @@ var router = express.Router()
 
 router.get('/', (req, res) => {
   var db = req.app.get('db')
-  db('circuits').then(function(circuits) {
-    console.log(circuits)
-    res.render('index', {circuits})
+  db('seasons').then((seasons) => {
+    res.render('index', {seasons})
   })
+})
 
+router.get('/circuits', (req, res) => {
+  var db = req.app.get('db')
+  db('circuits').then(function(circuits) {
+    res.render('circuits', {circuits})
+  })
+})
+
+router.get('/season/:id', (req, res) => {
+  var db = req.app.get('db')
+  var id = req.params.id
+  db('races')
+    .select('races.year as season-year', '*')
+    .join('seasons', 'seasons.year', '=', 'races.year')
+    .where('races.year', id)
+
+    .then((season) => {
+
+
+        console.log(season);
+        res.render('season', {season})
+
+
+  })
 })
 
 module.exports = router
