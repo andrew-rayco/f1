4f304d890096790ec328c9422b49fa5fae5bdad8
Andrew <andy@andycondon.com>
2017-07-05 22:21:24 +1200

    return external api res undefined

--- server/apiRoutes.js
+++ server/apiRoutes.js
@@ -2,14 +2,40 @@
 var express = require('express')
 var router = express.Router()
 
-// example json api
+var request = require('superagent')
+
 router.get('/', (req, res) => {
-  var db = req.app.get('db')
-  db('seasons')
-    .orderBy('year', 'asc')
-    .then((seasons) => {
-    res.json(seasons)
-  })
+  request
+    .get('http://ergast.com/api/f1/seasons')
+    .end((err, result) => {
+      if(err) {
+        console.log(err)
+      } else {
+        console.log(Object.keys(res))
+        console.log(res.req)
+      }
+    })
 })
+
+// example json api
+// router.get('/', (req, res) => {
+//   var db = req.app.get('db')
+//   db('seasons')
+//     .orderBy('year', 'asc')
+//     .then((seasons) => {
+//     res.json(seasons)
+//   })
+// })
+
+// experiment with external api
+// router.get('/', (req, res) => {
+//   var ourData;
+//   var ourRequest = new XMLHttpRequest();
+//   ourRequest.open("GET", "https://eda-te-reo.herokuapp.com/api/proverbs", false);
+//   ourRequest.onload= function () {
+//     ourData = JSON.parse(ourRequest.responseText);
+//   };
+//   ourRequest.send();
+// })
 
 module.exports = router
