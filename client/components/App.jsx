03b3bb89df148859ec98feab3366f3978562144d
Andrew <andy@andycondon.com>
2017-07-14 20:53:55 +1200

    debugging reactInternalInstances error

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
+import {browserHistory} from 'react-router'
 
 import RunRace from './RunRace'
 import Home from './Home'
@@ -7,24 +8,26 @@ import Header from './Header'
 import Circuits from './Circuits'
 import FullSeason from './FullSeason'
 
-
 export default class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-    }
+    this.state = {}
   }
 
-  render () {
+  render() {
     return (
-      <Router>
+      <Router history={browserHistory}>
         <div className="container">
           <Header />
-          <Route path = '/' exact={true} component = {Home} />
-          <Route path = '/circuits' exact={true} component = {Circuits} />
-          <Route path = '/season/:id' component = {FullSeason} />
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/circuits' exact={true} component={Circuits}/>
+          <Route path='/season/:id' component={FullSeason}/>
         </div>
       </Router>
     )
   }
 }
+
+// <Header/>
+// <Route path='/circuits' exact={true} component={Circuits}/>
+// <Route path='/season/:id' component={FullSeason}/>
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Header = () => {
-  
+
   return (
     <div className="header">
       {console.log('header')}
       <nav>
-        <a id="home-link" href="/"><img id="home-link-img" src="/images/car-formula-1.png" alt="F1 car silhouette" /></a>
+        <a id="home-link" href="/#/"><img id="home-link-img" src="/images/car-formula-1.png" alt="F1 car silhouette" /></a>
       </nav>
       <h1>Formula 1 Database</h1>
     </div>
