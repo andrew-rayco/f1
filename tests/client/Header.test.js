b909c36103fcf1243926c205ad2550c2fb527397
Andrew <andy@andycondon.com>
2017-10-09 17:18:24 +1300

    add initial unit tests

---
+++ tests/client/Header.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Header from '../../client/components/Header'
+
+describe('Header', () => {
+  let props = { location: {pathname:"/season/2014"}}
+  const app = shallow(<Header {...props} />)
+
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot()
+  })
+
+  it('contains an h1', () => {
+    expect(app.find('h1').exists()).toBe(true)
+  })
+
+  it('contains an h1 with the correct text', () => {
+    expect(app.find('h1').text()).toEqual('F1ian')
+  })
+})
