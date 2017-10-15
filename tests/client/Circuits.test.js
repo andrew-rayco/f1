39bc57623bf1970dab3d86c96e280fd60f15fdfe
Andrew <andy@andycondon.com>
2017-10-15 15:32:51 +1300

    testing Circuits component

--- tests/client/Circuits.test.js
+++ tests/client/Circuits.test.js
@@ -3,49 +3,60 @@ import { shallow } from 'enzyme'
 import Circuits from '../../client/components/Circuits'
 
 describe('Circuits', () => {
-  const app = shallow(<Circuits />)
-  const circuits = [
-    {
-      "circuitId":25,
-      "circuitRef":"galvez",
-      "name":"Autódromo Juan y Oscar Gálvez",
-      "location":"Buenos Aires",
-      "country":"Argentina",
-      "lat":-34.6943,
-      "lng":-58.4593,
-      "alt":null,
-      "url":"http://en.wikipedia.org/wiki/Aut%C3%B3dromo_Oscar_Alfredo_G%C3%A1lvez"
-    },
-    {
-      "circuitId":1,
-      "circuitRef":"albert_park",
-      "name":"Albert Park Grand Prix Circuit",
-      "location":"Melbourne",
-      "country":"Australia",
-      "lat":-37.8497,
-      "lng":144.968,
-      "alt":10,
-      "url":"http://en.wikipedia.org/wiki/Melbourne_Grand_Prix_Circuit"
-    }
-  ]
+  const mockListCircuits = jest.fn()
+  const props = { listCircuits: mockListCircuits }
+  const app = shallow(<Circuits {...props} />)
+  // const circuits = [
+  //   {
+  //     "circuitId":25,
+  //     "circuitRef":"galvez",
+  //     "name":"Autódromo Juan y Oscar Gálvez",
+  //     "location":"Buenos Aires",
+  //     "country":"Argentina",
+  //     "lat":-34.6943,
+  //     "lng":-58.4593,
+  //     "alt":null,
+  //     "url":"http://en.wikipedia.org/wiki/Aut%C3%B3dromo_Oscar_Alfredo_G%C3%A1lvez"
+  //   },
+  //   {
+  //     "circuitId":1,
+  //     "circuitRef":"albert_park",
+  //     "name":"Albert Park Grand Prix Circuit",
+  //     "location":"Melbourne",
+  //     "country":"Australia",
+  //     "lat":-37.8497,
+  //     "lng":144.968,
+  //     "alt":10,
+  //     "url":"http://en.wikipedia.org/wiki/Melbourne_Grand_Prix_Circuit"
+  //   }
+  // ]
 
-  // it('renders correctly', () => {
-  //   expect(app).toMatchSnapshot()
+  // beforeEach(() => {
+  //   app.setState({circuits})
   // })
 
-  // it('contains the `circuit-list` div', () => {
-  //   expect(wrapper.find('.circuit-list').exists()).toBe(true)
-  // })
-
-
-  // it('calls listCircuits', () => {
-  //   const mockListCircuits = jest.fn()
-  //   expect().toHaveBeenCalled()
-  // })
-
-  it('just passes the damn test for now', () => {
-    expect(3).toEqual(3)
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot()
   })
 
+  it('contains the `circuit-list` div', () => {
+    expect(app.find('.circuit-list').exists()).toBe(true)
+  })
+
+  // it('has a tbody with multiple children', () => {
+  //   expect(app.find('tr').length).toBeGreaterThan(10)
+  // })
+
+  // it('mock setTimeout test', () => {
+  //  jest.useFakeTimers();
+  //  setTimeout(() => {
+  //    expect(app.state.circuits.length).toBeGreaterThan(10);
+  //  }, 500);
+  //  jest.runAllTimers();
+  // });
+
+  it('starts with state.circuits null', () => {
+    expect(app.state.circuits).toEqual(undefined)
+  })
 
 })
