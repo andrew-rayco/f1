d74d466e6791435c677241beca030f820f38ef11
Andrew <andy@andycondon.com>
2017-10-16 11:14:33 +1300

    need to delay testing until state data is fetched

--- tests/client/FullSeason.test.js
+++ tests/client/FullSeason.test.js
@@ -5,13 +5,15 @@ import FullSeason from '../../client/components/FullSeason'
 describe('FullSeason component', () => {
   let props = { location: {pathname:"/season/2014"}, year: 1985 }
   let races = [{"season-year":2014,"raceUrl":"https://en.wikipedia.org/wiki/2014_Australian_Grand_Prix","raceName":"Australian Grand Prix","raceId":900,"year":2014,"round":1,"circuitId":1,"name":"Australian Grand Prix","date":"2014-03-16","time":"06:00:00","url":"http://en.wikipedia.org/wiki/2014_Formula_One_season"},{"season-year":2014,"raceUrl":"https://en.wikipedia.org/wiki/2014_Malaysian_Grand_Prix","raceName":"Malaysian Grand Prix","raceId":901,"year":2014,"round":2,"circuitId":2,"name":"Malaysian Grand Prix","date":"2014-03-30","time":"08:00:00","url":"http://en.wikipedia.org/wiki/2014_Formula_One_season"}]
-  // const app = shallow(<FullSeason {...props} />)
+  beforeAll(() => {
+    const app = shallow(<FullSeason {...props} />)
+    app.setState({races: races})
+  })
   //
   //
-  // it('renders correctly', () => {
-  //   app.setState({ races: [{ year: 1985 }, { year: 2015 }, 3]})
-  //   expect(app).toMatchSnapshot()
-  // })
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot()
+  })
 
   it('just passes the damn test for now', () => {
     expect(3).toEqual(3)
